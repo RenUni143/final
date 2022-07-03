32b913f5861d33df07c4663c310a773384094aa0
Ren <uniren143@gmail.com>
2022-07-02 22:55:47 -0500

    Mejora de Historia de usuario

--- src/logica/Sorteo.py
+++ src/logica/Sorteo.py
@@ -4,16 +4,13 @@ from src.modelo.declarative_base import engine, Base, session
 
 def agregar_asignatura(idAsignatura):
     busqueda = session.query(Asignatura).filter(Asignatura.id == idAsignatura).first()
-    if len(busqueda) == 1:
+
+    if busqueda:
         session.query(Asignatura).filter(Asignatura.id == idAsignatura).update({Asignatura.nombasig: 'construccion de '
                                                                                                      'software'})
         print("editado")
         session.commit()
         session.close()
-
-        return True
-    elif len(busqueda) == 0:
-        print("la asignatura con el id ", idAsignatura, "no existe")
 
 
 class Sorteo():
